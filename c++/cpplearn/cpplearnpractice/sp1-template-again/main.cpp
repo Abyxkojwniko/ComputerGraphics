ea81cbc973ad9e403ae2682b3ac3dd6296af2797
Abyxkojw <abyxkojw@163.com>
2025-10-16 11:41:31 +0800

    update ssfinae

--- c++/cpplearn/cpplearnpractice/sp1-template-again/main.cpp
+++ c++/cpplearn/cpplearnpractice/sp1-template-again/main.cpp
@@ -24,22 +24,49 @@ int main(){
     // std::list<int> l1 = {10, 20, 30};
     // ListPrint.print(l1);
 
-    Printer<int> intPrinter;
-    intPrinter.print(10);
-    Printer<std::string> strPrinter;
-    strPrinter.print("hello"); 
-    int a = 10;
-    int* b = &a;
-    Pair<int, int*> Pair1(15, b);
-    std::cout << "A address: " << &a << std::endl;
-    Pair1.print();
-    std::cout << "---------------------------------------------" << std::endl;
-    printValue(10);
-    std::string s1 = "Hello World C++ Template";
-    printValue(s1);
-    printValue(b);
-    printAll(1, 2.5, "hello", 'A');
-    coutAll(10, 20, 10.5, "abc");
-    std::cout << "Sum is : " << std::fixed << std::setprecision(10)  << sumAll(1.0, 2.5, 3.66, 1354, 114514) << std::endl;
+    // Printer<int> intPrinter;
+    // intPrinter.print(10);
+    // Printer<std::string> strPrinter;
+    // strPrinter.print("hello"); 
+    // int a = 10;
+    // int* b = &a;
+    // Pair<int, int*> Pair1(15, b);
+    // std::cout << "A address: " << &a << std::endl;
+    // Pair1.print();
+    // std::cout << "---------------------------------------------" << std::endl;
+    // printValue(10);
+    // std::string s1 = "Hello World C++ Template";
+    // printValue(s1);
+    // printValue(b);
+    // printAll(1, 2.5, "hello", 'A');
+    // coutAll(10, 20, 10.5, "abc");
+    // std::cout << "Sum is : " << std::fixed << std::setprecision(10)  << sumAll(1.0, 2.5, 3.66, 1354, 114514) << std::endl;
+    
+    // int a = 10;
+    // print_type(a);
+    // float b = 3.1415926;
+    // print_type(b);
+    // const char* cstr = "Hello";
+    // print_type(cstr);
+    // std::string str = "Hello world";
+    // print_type(str);
+    // int* c = &a;
+    // print_type(c);
+    // double x = 3.356523;
+    // print_type(x);
+    // cout_type(a);
+    // cout_type(b);
+    // cout_type(cstr);
+    // cout_type(str);
+    // cout_type(c);
+
+    WithFoo wf;
+    call_foo(wf);
+
+    TypePrinter<WithFoo>::print();
+
+    TypePrinter<WithValueType>::print();
+    TypePrinter<WithValueType> print_type;
+    call_foo(print_type);
     return 0;
 }
--- c++/cpplearn/cpplearnpractice/sp1-template-again/template.h
+++ c++/cpplearn/cpplearnpractice/sp1-template-again/template.h
@@ -5,6 +5,9 @@
 #include <vector>
 #include <list>
 #include <iomanip>
+#include <type_traits>
+#include <concepts>
+#include <vector>
 
 template <typename T>
 T maxValue(T a, T b){
@@ -125,3 +128,211 @@ template <typename... Args>
 auto sumAll(Args... args) ->decltype((args + ...)){
     return (args + ...);
 }
+
+
+//sfinae使用举例
+template<typename T>
+typename std::enable_if<std::is_integral<T>::value, void>::type
+print_type(T value){
+    std::cout << "Integral type" << std::endl;
+}
+
+
+template<typename T>
+typename std::enable_if<std::is_floating_point<T>::value, void>::type
+print_type(T value){
+    std::cout << "Floating point type" << std::endl;
+}
+
+template<typename T>
+typename std::enable_if<std::is_same<T, const char*>::value ||
+    std::is_same<T, char*>::value, void>::type
+print_type(T value){
+    std::cout << "Cstyle string: " << value << std::endl;
+}
+
+template<typename T>
+typename std::enable_if<std::is_same<T, std::string>::value, void>::type
+print_type(T value){
+    std::cout << "String: " << std::endl;
+}
+
+template<typename T>
+typename std::enable_if<std::is_pointer<T>::value &&
+    !std::is_same<T, const char*>::value &&
+    !std::is_same<T, char*>::value>::type
+print_type(T value){
+    std::cout << "Pointer type" << std::endl;
+}
+
+template<typename T>
+typename std::enable_if<
+    !std::is_integral<T>::value &&
+    !std::is_floating_point<T>::value &&
+    !std::is_same<T, const char*>::value &&
+    !std::is_same<T, char*>::value &&
+    !std::is_same<T, std::string>::value &&
+    !(std::is_pointer<T>::value &&
+    !std::is_same<T, const char*>::value &&
+    !std::is_same<T, char*>::value), void
+    >::type 
+print_type(T value){
+    std::cout << "Other type" << std::endl;
+}
+
+template<typename T>
+concept Integral = std::is_integral_v<T>;
+
+template<typename T>
+concept FloatingPoint = std::is_floating_point_v<T>;
+
+template<typename T>
+concept CString = std::is_same_v<T, const char*> || std::is_same_v<T, char*>;
+
+template<typename T>
+concept StdString = std::is_same_v<T, std::string>;
+
+template<typename T>
+concept Pointer = std::is_pointer_v<T> && !(std::is_same_v<T, const char*> || std::is_same_v<T, char*>);
+
+void cout_type(Integral auto value){
+    std::cout << "Integral type" << std::endl;
+}
+
+void cout_type(FloatingPoint auto value){
+    std::cout << "Floating type" << std::endl;
+}
+
+void cout_type(CString auto value){
+    std::cout << "CString type" << std::endl;
+}
+
+void cout_type(StdString auto value){
+    std::cout << "String type" << std::endl;
+}
+
+void cout_type(Pointer auto value){
+    std::cout << "Pointer type" << std::endl;
+}
+
+template<typename T>
+requires (!Integral<T> && !FloatingPoint<T> && !CString<T> && !StdString<T> && !Pointer<T>)
+void cout_type(T value){
+    std::cout << "Other Type" << std::endl;
+}
+
+
+template<typename T>
+class has_foo{
+private:
+    typedef char yes[1];
+    typedef char no[2];
+    template<typename U, void(U::*)()>
+    struct SFINAE{};
+    template<typename U>
+    static yes& test(SFINAE<U, &U::foo>*);
+    template <typename U>
+    static no& test(...);
+public:
+    static constexpr bool value = sizeof(test<T>(0)) == sizeof(yes);
+
+};
+
+
+template <typename T>
+typename std::enable_if<has_foo<T>::value, void>::type
+call_foo(T& obj){
+    obj.foo();
+    std::cout << "foo() called" << std::endl;
+}
+
+class WithFoo{
+public:
+    void foo(){
+        std::cout << "WithFoo::foo()" << std::endl;
+    }
+};
+
+class WithOutFoo{};
+
+
+template <typename T, typename Enable = void>
+struct has_non_void_value_type : std::false_type{};
+
+//当T有成员类型的时候判断为true，也就是T有具体的成员类型
+template <typename T>
+struct has_non_void_value_type<T, std::enable_if_t<!std::is_void_v<typename T::value_type>>>:std::true_type{};
+
+template<typename T, bool HasValueType = has_non_void_value_type<T>::value>
+struct TypePrinter;
+
+// 特化：当 T 有成员类型 `value_type`
+template <typename T>
+struct TypePrinter<T, true> {
+    void foo(){
+        std::cout << "WithFoo::foo()" << std::endl;
+    }
+    static void print() {
+        std::cout << "T has a member type 'value_type'." << std::endl;
+    }
+};
+
+template<typename T>
+struct TypePrinter<T, false>{
+    static void print(){
+        std::cout << "T does not have a member type value_type" << std::endl;
+    }
+};
+
+struct WithValueType{
+    using value_type = int;
+};
+
+struct WithoutValueType{};
+
+
+//Concept版本
+// struct WithVoidValueType {
+//     using value_type = void; // 用于测试非 void 约束
+// };
+
+// // ----------------------------------------------------
+// // 2. Concept 定义 (替代 has_non_void_value_type 结构体)
+// // ----------------------------------------------------
+// template <typename T>
+// concept HasNonVoidValueType = requires {
+//     // 检查 T 是否有一个名为 value_type 的成员类型
+//     typename T::value_type;
+    
+//     // 检查这个 value_type 是否不是 void
+//     requires !std::is_void_v<typename T::value_type>;
+// };
+
+// // ----------------------------------------------------
+// // 3. TypePrinter 结构 (使用 Concept 进行约束分派)
+// // ----------------------------------------------------
+
+// // A. TypePrinter 的主要定义（作为不满足约束时的默认回退）
+// // 对应原代码中的 TypePrinter<T, false>
+// template <typename T>
+// struct TypePrinter {
+//     static void print() {
+//         std::cout << "T does not have a member type value_type (Concept version)" << std::endl;
+//     }
+//     // 注意：此版本不提供 foo()
+// };
+
+// // B. 特化/约束版本 (只适用于满足 Concept 的类型)
+// // 对应原代码中的 TypePrinter<T, true>
+// template <HasNonVoidValueType T>
+// struct TypePrinter<T> {
+//     void foo() {
+//         std::cout << "WithFoo::foo() (Concept version)" << std::endl;
+//     }
+//     static void print() {
+//         std::cout << "T has a member type 'value_type'. (Concept version)" << std::endl;
+//     }
+// };
+
+
+
